5aad3503e15d5b71777c21bf63ac06d54fe21270
Martan03 <46300167+Martan03@users.noreply.github.com>
2022-10-14 21:39:40 +0200

    Add project files.

---
+++ LanCom/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+public class SocketListener
+{
+    public static int Main(string[] args)
+    {
+        switch (args[0])
+        {
+            case "server":
+                StartServer();
+                break;
+            case "client":
+                StartClient();
+                break;
+            default:
+                break;
+        }
+        return 0;
+    }
+
+    public static void StartServer()
+    {
+        IPHostEntry host = Dns.GetHostEntry("localhost");
+        IPAddress ipAddress = host.AddressList[0];
+        IPEndPoint localEndPoint = new (ipAddress, 11000);
+
+        try
+        {
+            Socket listener = new (ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            listener.Bind(localEndPoint);
+            listener.Listen(1);
+
+            Console.WriteLine("Waiting for a connection...");
+            Socket handler = listener.Accept();
+
+            string data = null;
+            byte[] bytes = null;
+
+            while (true)
+            {
+                bytes = new byte[1024];
+                int bytesRec = handler.Receive(bytes);
+                data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+
+                if (data.IndexOf("<EOF>") > -1)
+                    break;
+            }
+            Console.WriteLine("Text received: {0}", data);
+
+            byte[] msg = Encoding.ASCII.GetBytes(data);
+            handler.Send(msg);
+            handler.Shutdown(SocketShutdown.Both);
+            handler.Close();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+
+        Console.WriteLine("\nPress any key to continue...");
+        Console.ReadKey();
+    }
+
+    public static void StartClient()
+    {
+        byte[] bytes = new byte[1024];
+
+        try
+        {
+            IPHostEntry host = Dns.GetHostEntry("localhost");
+            IPAddress ipAddress = host.AddressList[0];
+            IPEndPoint remoteEP = new(ipAddress, 11000);
+
+            Socket sender = new(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                sender.Connect(remoteEP);
+
+                Console.WriteLine("Socket connected to {0}", sender.RemoteEndPoint.ToString());
+
+                byte[] msg = Encoding.ASCII.GetBytes("This is a test<EOF>");
+
+                int bytesSent = sender.Send(msg);
+
+                int bytesRec = sender.Receive(bytes);
+                Console.WriteLine("Echoed test = {0}", Encoding.ASCII.GetString(bytes, 0, bytesRec));
+
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+                sender.Close();
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("ArgumentNullException: {0}", e.ToString());
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("SocketException: {0}", e.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected exception: {0}", e.ToString());
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+}
